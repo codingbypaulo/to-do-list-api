c3690c6745846a01c0c1dd4924134229c91c791b
Paulo Santos <codingbypaulo@gmail.com>
2025-01-28 19:49:42 -0300

    install cors and swagger

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { swagger } from "@elysiajs/swagger";
 import chalk from "chalk";
 
-const server = new Elysia();
+const server = new Elysia()
+  .use(cors())
+  .use(swagger());
 
 server.listen(process.env.PORT!, async () => {
   console.log(chalk.green("Server running!"));

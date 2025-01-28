2364d8cbbd9ef32f95824b61a426f5a679da9b8f
Paulo Santos <codingbypaulo@gmail.com>
2025-01-28 20:53:51 -0300

    implement user routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,10 +2,12 @@ import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { swagger } from "@elysiajs/swagger";
 import chalk from "chalk";
+import { user } from "../routes/user";
 
 const server = new Elysia()
   .use(cors())
-  .use(swagger());
+  .use(swagger())
+  .use(user);
 
 server.listen(process.env.PORT!, async () => {
   console.log(chalk.green("Server running!"));
---
+++ src/routes/user.ts
@@ -0,0 +1,95 @@
+import { Elysia, t } from "elysia";
+import { jwt } from "@elysiajs/jwt";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const schema = t.Object({
+  sub: t.String(),
+});
+
+export const user = new Elysia()
+  .use(
+    jwt({
+        name: "jwt",
+        secret: process.env.SECRET!,
+        schema,
+    })
+  )
+  .post("/user", async ({ body, jwt, cookie: { auth }, set }) => {
+    const { email, password } = body;
+
+    const userFound = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!userFound) {
+      const passwordHash = await Bun.password.hash(password);
+
+      const userCreated = await prisma.user.create({
+        data: {
+          email,
+          password: passwordHash,
+        },
+      });
+
+      if (userCreated) {
+        const { id } = userCreated;
+
+        auth.set({
+          value: await jwt.sign({ sub: id }),
+          httpOnly: true,
+          maxAge: 7 * 86400,
+          path: '/',
+        })
+
+        set.status = "OK";
+      } else {
+        set.status = "Conflict";
+      };
+    } else {
+      set.status = "Conflict";
+    };
+  }, {
+    body: t.Object({
+      email: t.String({ format: "email", }),
+      password: t.String({ minLength: 8, }),
+    }),
+  })
+  .post("/user/login", async ({ body, jwt, cookie: { auth }, set }) => {
+    const { email, password } = body;
+
+    const userFound = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userFound) {
+      const { id, password: passwordHash } = userFound;
+
+      const passwordIsMatch = await Bun.password.verify(password, passwordHash);
+
+      if (passwordIsMatch) {
+        auth.set({
+          value: await jwt.sign({ sub: id }),
+          httpOnly: true,
+          maxAge: 7 * 86400,
+          path: '/',
+        })
+
+        set.status = "OK";
+      } else {
+        set.status = "Unauthorized";
+      };
+    } else {
+      set.status = "Not Found";
+    };
+  }, {
+    body: t.Object({
+      email: t.String({ format: "email", }),
+      password: t.String({ minLength: 8, }),
+    }),
+  });

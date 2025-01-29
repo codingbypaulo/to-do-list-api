6bd7919785ad1f4d00463de64d9c48e0b5ecb851
Paulo Santos <codingbypaulo@gmail.com>
2025-01-28 21:39:02 -0300

    implement task routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,11 +3,13 @@ import { cors } from "@elysiajs/cors";
 import { swagger } from "@elysiajs/swagger";
 import chalk from "chalk";
 import { user } from "../routes/user";
+import { task } from "../routes/task";
 
 const server = new Elysia()
   .use(cors())
   .use(swagger())
-  .use(user);
+  .use(user)
+  .use(task);
 
 server.listen(process.env.PORT!, async () => {
   console.log(chalk.green("Server running!"));
---
+++ src/routes/task.ts
@@ -0,0 +1,66 @@
+import { Elysia, t } from "elysia";
+import { jwt } from "@elysiajs/jwt";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const schema = t.Object({
+  id: t.String(),
+});
+
+export const task = new Elysia()
+  .use(
+    jwt({
+        name: "jwt",
+        secret: process.env.SECRET!,
+        schema,
+    })
+  )
+  .get('/task', async ({ jwt, set, cookie: { auth } }) => {
+    const user = await jwt.verify(auth.value);
+
+    if (user) {
+      const { id } = user;
+
+      const allTasks = await prisma.task.findMany({
+        where: {
+          userId: id,
+        },
+      });
+
+      if (allTasks) {
+        return allTasks;
+      } else {
+        set.status = 'Internal Server Error';
+      };
+    } else {
+      set.status = 'Unauthorized';
+    };
+  })
+  .post('/task', async ({ body, jwt, set, cookie: { auth } }) => {
+    const user = await jwt.verify(auth.value);
+
+    if (user) {
+      const { id } = user;
+      const { content } = body;
+
+      const taskCreated = await prisma.task.create({
+        data: {
+          content,
+          userId: id,
+        },
+      });
+
+      if (taskCreated) {
+        set.status = 'OK';
+      } else {
+        set.status = 'Internal Server Error';
+      };
+    } else {
+      set.status = 'Unauthorized';
+    };
+  }, {
+    body: t.Object({
+      content: t.String(),
+    }),
+  });
